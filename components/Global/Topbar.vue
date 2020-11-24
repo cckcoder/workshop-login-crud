4a55742c48e6eecddfe227f6421cb26cdf743b31
codewiz <c.chaiwat.ch@gmail.com>
2020-11-24 15:23:10 +0000

    init commit
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

---
+++ components/Global/Topbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="toggleDrawer" />
+    <v-toolbar-title v-text="title" />
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleDrawer () {
+      this.$store.commit('set_drawer', ! this.$store.state.drawer)
+    }
+  },
+  data () {
+    return {
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app dark>
+    <Sidebar></Sidebar>
+    <Topbar></Topbar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from '@/components/Global/Sidebar'
+import Topbar from '@/components/Global/Topbar'
+import Footer from '@/components/Global/Footer'
+
+export default {
+  components: {
+    Sidebar,
+    Topbar,
+    Footer
+  },
+  computed: {
+    drawer: {
+      get () {
+        return this.$nuxt.$store.state.drawer
+      },
+      set (val) {
+        console.warn("Old Val", this.$nuxt.$store.state.drawer)
+        console.warn("New Val", val)
+        this.$store.commit('set_drawer', val)
+        console.warn("New Val", this.$nuxt.$store.state.drawer)
+      }
+    }
+  }
+}
+</script>
